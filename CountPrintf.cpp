fa5a27c2c9c20c4232cd99ab55ff810d7a1e79d9
Rishit_India <rishitsanmukhani@gmail.com>
2016-10-02 16:43:45 +0530

    Count Printf pass

--- CountPrintf.cpp
+++ CountPrintf.cpp
@@ -12,9 +12,20 @@ struct CntPrintf : public FunctionPass{
     static char ID;
     CntPrintf() : FunctionPass(ID) {}
     bool runOnFunction(Function &F) override {
-        cerr << "i iz not error!"<<endl;
-        errs() << "Hello: ";
-        errs().write_escaped(F.getName()) << '\n';
+    	  int count = 0;
+		    for(Function::iterator it_f = F.begin(); it_f != F.end() ; ++it_f){
+		      BasicBlock& b = *it;
+		      for(BasicBlock::iterator it_b = b.begin(); it_b != b.end(); ++it_b){
+				    CallInst *si = dyn_cast<CallInst>(&*it_b);
+		        if(si != NULL){
+		        	Function *called_f = si->getCalledFunction();
+		        	if(called_f != NULL){
+		        		if(called_f->getName().str().compare("printf")==0)count++;
+		        	}
+		        }
+		      }
+		    }
+				errs().write_escaped(F.getName())<<":\nPrintf(): "<<count<<" calls\n";
         return false;
     }
 };
