4a0eaa899cdec56b88e379316ab484afa9cc9a66
Yash Gupta <yashg2@gmail.com>
2016-10-02 17:40:59 +0530

    part1 done

--- CountPrintf.cpp
+++ CountPrintf.cpp
@@ -1,7 +1,7 @@
 #include "llvm/Pass.h"
 #include "llvm/IR/Function.h"
 #include "llvm/Support/raw_ostream.h"
-
+#include "llvm/IR/Instructions.h"
 #include <iostream>
 
 using namespace llvm;
@@ -12,9 +12,19 @@ struct CntPrintf : public FunctionPass{
     static char ID;
     CntPrintf() : FunctionPass(ID) {}
     bool runOnFunction(Function &F) override {
-        cerr << "i iz not error!"<<endl;
-        errs() << "Hello: ";
-        errs().write_escaped(F.getName()) << '\n';
+        int num_calls = 0;
+        for(Function::iterator blk_it = F.begin(); blk_it != F.end(); blk_it++){
+            for(BasicBlock::iterator inst_it = blk_it->begin(); inst_it != blk_it->end();
+              inst_it++){
+                CallInst *call_inst = dyn_cast<CallInst>(&(*inst_it));
+                if(call_inst != NULL){
+                    Function *func = call_inst->getCalledFunction();
+                    if(func->getName().equals("printf"))
+                        num_calls++;
+                }
+            }
+        }
+        errs().write_escaped(F.getName()) << ":\nprintf(): "<<num_calls<<" calls\n";
         return false;
     }
 };
